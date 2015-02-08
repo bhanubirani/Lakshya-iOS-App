90940dc9f3871d15e50156309764a16ee9449e1a
bhanubirani <bhanu@inkoniq.com>
2015-02-08 13:54:51 +0530

    added project model

---
+++ Lakshya/Lakshya/Classes/Models/LKProject.m
@@ -0,0 +1,13 @@
+//
+//  LKProject.m
+//  Lakshya
+//
+//  Created by Bhanu Birani on 08/02/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import "LKProject.h"
+
+@implementation LKProject
+
+@end
