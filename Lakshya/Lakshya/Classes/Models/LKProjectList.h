67fdccd1cc155e3b9aee3b71b566f12ce12b30dd
Rahul Borawar <rahul.borawar@commonfloor.com>
2015-02-08 15:01:08 +0530

    adding project list model

---
+++ Lakshya/Lakshya/Classes/Models/LKProjectList.h
@@ -0,0 +1,13 @@
+//
+//  LKProjectList.h
+//  Lakshya
+//
+//  Created by Rahul on 2/8/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LKProjectList : NSObject
+
+@end
---
+++ Lakshya/Lakshya/Classes/Models/LKProjectList.m
@@ -0,0 +1,14 @@
+//
+//  LKProjectList.m
+//  Lakshya
+//
+//  Created by Rahul on 2/8/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import "LKProjectList.h"
+#import "LKProject.h"
+
+@implementation LKProjectList
+
+@end
