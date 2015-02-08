6a7131145e08854799089ac8f92545b0fc6bc9e6
bhanubirani <bhanu@inkoniq.com>
2015-02-08 13:19:27 +0530

    added project list manager class

---
+++ Lakshya/Lakshya/Classes/Managers/LKProjectListManger.h
@@ -0,0 +1,13 @@
+//
+//  LKProjectListManger.h
+//  Lakshya
+//
+//  Created by Bhanu Birani on 08/02/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LKProjectListManger : NSObject
+
+@end
---
+++ Lakshya/Lakshya/Classes/Managers/LKProjectListManger.m
@@ -0,0 +1,13 @@
+//
+//  LKProjectListManger.m
+//  Lakshya
+//
+//  Created by Bhanu Birani on 08/02/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import "LKProjectListManger.h"
+
+@implementation LKProjectListManger
+
+@end
