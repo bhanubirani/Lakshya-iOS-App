0bbd6de026ad9735ed75c55a72020acd11b6a046
bhanubirani <bhanu@inkoniq.com>
2015-02-08 10:43:00 +0530

    first-commit

---
+++ Lakshya/Lakshya/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lakshya
+//
+//  Created by Bhanu Birani on 08/02/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
