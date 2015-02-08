466c4379e70058f72dfad3d9bdbcc47c5ffd9947
Rahul Borawar <rahul.borawar@commonfloor.com>
2015-02-08 13:07:17 +0530

    Merging some changes

---
+++ Lakshya/Lakshya/Classes/ViewController/Landing/LKProjectListCVCell.h
@@ -0,0 +1,16 @@
+//
+//  LKProjectListCVCell.h
+//  Lakshya
+//
+//  Created by Rahul on 2/8/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKProjectListCVCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *projectImageView;
+@property (weak, nonatomic) IBOutlet UILabel *projectNameLabel;
+
+@end
---
+++ Lakshya/Lakshya/Classes/ViewController/Landing/LKProjectListCVCell.m
@@ -0,0 +1,17 @@
+//
+//  LKProjectListCVCell.m
+//  Lakshya
+//
+//  Created by Rahul on 2/8/15.
+//  Copyright (c) 2015 Lakshya. All rights reserved.
+//
+
+#import "LKProjectListCVCell.h"
+
+@interface LKProjectListCVCell()
+
+@end
+
+@implementation LKProjectListCVCell
+
+@end
